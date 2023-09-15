fe167ee5e46b53370b59d592bbbd7b9a1b253a90
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-09-15 19:26:25 +0000

    Conversion of dataTypes

---
+++ 01_basics_js/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = undefined
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); 
+// console.log( valueInNumber);
+
+// "33" => 33
+// "33xyz" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 'abs';
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// '' => false
+// 'abs' => true
+
+let num = 45
+let strNum = String(num)
+console.log(typeof strNum); 
