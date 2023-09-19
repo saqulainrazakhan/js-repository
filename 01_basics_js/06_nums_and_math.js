44558eff43299f76102cb6200db8db9e2a97f09e
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-09-19 07:19:17 +0000

    math and num

---
+++ 01_basics_js/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 500
+// console.log(score);
+
+const balance = new Number(700)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 124.9876
+
+// console.log(otherNum.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+console.log(Math) // Math is an object in JS and it different  methods through which we can perform different operation on it 
+// console.log(Math.round(4.6));
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,3,6));
+// console.log(Math.max(9,8,7,4,12));
+
+
+console.log(Math.floor(Math.random()));
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+
+
