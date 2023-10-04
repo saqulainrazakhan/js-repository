a110407ea7623216dabf085b18b7a119413d2190
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-10-04 07:33:26 +0000

    switch control flow syntax and concept

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch case syntax
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 2
+switch (month) {
+    case 1:
+        console.log('January');
+        break; // break is used to break the control flow
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+
+    default:
+        console.log('default case match');
+        break;
+}
