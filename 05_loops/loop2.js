246cac38eca3374e798883f52679a7a026728885
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-10-04 07:37:17 +0000

    while do.. while loop concept

---
+++ 05_loops/loop2.js
@@ -0,0 +1,20 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let fruits = ['Mango', 'Apple', 'Pomegranate', 'Banana', 'Watermelon']
+// let index = 0
+// while (index < fruits.length) {
+//     console.log(`Fruit is ${fruits[index]}`);
+//     index = index + 1
+// }
+
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
