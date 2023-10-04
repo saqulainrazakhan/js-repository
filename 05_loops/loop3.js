debf554362299bc28e3b4abab62daa52658dc586
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-10-04 07:38:26 +0000

    for loop and map constructor method

---
+++ 05_loops/loop3.js
@@ -0,0 +1,19 @@
+// for of
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings = 'Hello world!'
+for (const greet of greetings) {
+    console.log(greet);
+}
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+console.log(map);
