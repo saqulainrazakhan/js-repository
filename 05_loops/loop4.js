1a397f74dffae26530ac9a4e6c5254b477250100
saqulainrazakhan <85983209+saqulainrazakhan@users.noreply.github.com>
2023-10-04 07:39:12 +0000

    loops on object

---
+++ 05_loops/loop4.js
@@ -0,0 +1,14 @@
+const myObj = {
+    js: 'JAvascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+
+for (const key in myObj) {
+    console.log(`${key} is shorcut for ${myObj[key]}`); 
+  }
+
+  const programming = ['js', 'rb', ]
+ 
